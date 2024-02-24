cbba6720a4179c7dec61d679cc1a2d190031d55d
Jun <143848758+IlikeCookieJK@users.noreply.github.com>
2024-02-23 17:56:21 -0800

    Update 4-borrowing.js

--- 4-borrowing.js
+++ 4-borrowing.js
@@ -7,6 +7,6 @@ Can you think of a way to call hasOwnProperty on an object that has its own prop
 let map = {one: true, two: true, hasOwnProperty: true};
 
 // !!! Fix the call code below, do not modify the object above. !!!
-// console.log(map.hasOwnProperty)
-console.log(Object.hasOwn(map,"one"));
+
+console.log(hasOwnProperty.call(map,"one"));
 // → true
